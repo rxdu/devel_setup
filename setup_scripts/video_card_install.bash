633fafbec18735b0f224fbd15caaf42d42d89fd2
rdu <ruixiang.du@gmail.com>
2017-10-09 11:38:46 -0400

    more updates

--- setup_scripts/video_card_install.bash
+++ setup_scripts/video_card_install.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# for Ubuntu 16.04
+sudo add-apt-repository -y ppa:graphics-drivers/ppa 
 sudo apt-get -y update
-sudo apt-get -y install nvidia-current
+sudo apt-get -y install nvidia-375
