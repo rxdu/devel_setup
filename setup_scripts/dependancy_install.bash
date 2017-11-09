2dceeb41551e3bcfca905e6740205eb88eba52dd
Ruixiang Du <ruixiang.du@gmail.com>
2017-11-09 10:52:54 -0500

    updated scripts

--- setup_scripts/dependancy_install.bash
+++ setup_scripts/dependancy_install.bash
@@ -7,10 +7,11 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y install aptitude curl
 sudo apt-get -y install build-essential git cmake 
 sudo apt-get -y install openjdk-8-jdk
-sudo apt-get -y install python-dev python-numpy python-pip
+sudo apt-get -y install python-dev python-pip 
+sudo apt-get -y install python-numpy  python-scipy python-matplotlib
 sudo apt-get -y install libboost-all-dev libeigen3-dev 
 sudo apt-get -y install libopencv-dev python-opencv
 sudo apt-get -y install doxygen
 
 sudo apt-get -y install libqglviewer-dev
-sudo apt-get -y install qt4-default
+sudo apt-get -y install qt5-default
--- setup_scripts/useful_software_install.bash
+++ setup_scripts/useful_software_install.bash
@@ -19,6 +19,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # install VS Code
+sudo apt-get -y install apt-transport-https
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
