e5168e7b702d857c7b20a2735462ede779d38eaf
Ruixiang Du <ruixiang.du@gmail.com>
2019-07-04 16:31:00 +0800

    added libncurses5-dev to default install

--- scripts/devel_install.bash
+++ scripts/devel_install.bash
@@ -11,6 +11,7 @@ sudo apt-get -y install libopencv-dev python-opencv
 sudo apt-get -y install libboost-all-dev libeigen3-dev 
 sudo apt-get -y install libcgal-dev
 sudo apt-get -y install libgsl2 libgsl-dev
+sudo apt-get -y install libncurses5-dev
 
 sudo apt-get -y install qt5-default
 sudo apt-get -y install libqglviewer-dev-qt5
