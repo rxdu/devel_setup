0435c4a76925a2463c1066dced12283b4aebfbc2
Ruixiang Du <ruixiang.du@westonrobot.com>
2019-11-23 15:42:31 +0800

    updated ros install script

--- scripts/ros_melodic_install.bash
+++ scripts/ros_melodic_install.bash
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-sudo apt install -y ros-melodic-joy
+# install ROS melodic
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install -y ros-melodic-desktop-full
+
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+# install additional packages
+sudo apt install -y ros-melodic-navigation 
+sudo apt install -y ros-melodic-teleop-twist-keyboard 
+sudo apt install -y ros-melodic-pointcloud-to-laserscan
+sudo apt install -y ros-melodic-video-stream-opencv
